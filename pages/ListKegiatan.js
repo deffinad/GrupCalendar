0063d0201f11c0bae9962cb6f00c7916685fd826
azisfaisal <azissfm@gmail.com>
2023-04-18 08:20:30 +0700

    ganti halaman list kegiatan

--- pages/ListKegiatan.js
+++ pages/ListKegiatan.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export const ListKegiatan = () => {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Halaman List Kegiatan</Text>
+            <Text>Halaman List Kegiatannnnn</Text>
         </View>
     )
 }
